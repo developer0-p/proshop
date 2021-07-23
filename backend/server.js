08accb48a5c320388eab38e48081dd34fbc4a746
Pablo Domínguez <pablo.dominguezb@gmail.com>
2021-07-24 00:48:41 +0200

    Environment variables & ES Modules in Node.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
 const app = express()
-const products = require('./data/products')
+import products from './data/products.js'
+
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -15,4 +18,6 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000...'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}...`))
