f07b3f24af0fd553c0004e3517fc61c75a6bf62a
Pablo Domínguez <pablo.dominguezb@gmail.com>
2021-10-30 10:36:48 +0200

    Admin user Delete

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,6 +9,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
   userListReducer,
+  userDeleteReducer,
 } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducers'
 
@@ -21,6 +22,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
