b194f22b4118ad61c0387f098a0564dc91040f6d
Pablo Domínguez <pablo.dominguezb@gmail.com>
2021-10-30 11:56:32 +0200

    User Edit Screen & Get User Details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -202,8 +202,8 @@ export const deleteUsers = (id) => async (dispatch, getState) => {
         Authorization: `Bearer ${userInfo.token}`,
       },
     }
-    // eslint-disable-next-line
-    const { data } = await axios.delete(`/api/users/${id}`, config)
+
+    await axios.delete(`/api/users/${id}`, config)
 
     dispatch({
       type: USER_DELETE_SUCCESS,
