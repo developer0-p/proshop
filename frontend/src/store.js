972da1ade23577d579d1a563599f94c2626da475
Pablo Domínguez <pablo.dominguezb@gmail.com>
2021-11-01 00:46:46 +0100

    Admin Delete Products

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
+import { productListReducer, productDetailsReducer, productDeleteReducer } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers.js'
 import {
   userDetailsReducer,
@@ -17,6 +17,7 @@ import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayRe
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
