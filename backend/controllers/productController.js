3717c3a64be68756d6ad85b35bb3d52b56af0b52
Pablo Domínguez <pablo.dominguezb@gmail.com>
2021-11-07 17:59:06 +0100

    Top Product Carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -140,4 +140,13 @@ const createReview = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createReview }
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const topProducts = await Product.find({}).sort({ rating: -1 }).limit(3)
+  res.json(topProducts)
+})
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createReview, getTopProducts }
